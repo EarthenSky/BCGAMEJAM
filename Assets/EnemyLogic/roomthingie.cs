4cea71fecf898dc418dc316dc16196531b66bf7d
Justin <justinl8@hotmail.ca>
2020-02-15 01:33:22 -0800

    enemy logic beta

---
+++ Assets/EnemyLogic/roomthingie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomthingie : MonoBehaviour
+{
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+     Instantiate(enemy,new Vector3(0,0,0),Quaternion.identity);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
