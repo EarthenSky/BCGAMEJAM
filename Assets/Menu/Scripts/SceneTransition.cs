0e1c0d1a4b90ea0c6f71a85c3dcd04ca118ca104
mudclip11 <60982544+mudclip11@users.noreply.github.com>
2020-02-15 02:52:45 -0800

    Menu Items
    Contains a folder with a script to transition screens. It is collision based so as to be used as a door.

---
+++ Assets/Menu/Scripts/SceneTransition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    //When player enters the scripted collisionbox they are transitioned to the next screen
+    //Change name "moveTo" to the name if the eventual gameplay scene
+    void OnTriggerEnter2D(Collider2D Other) 
+    {
+        SceneManager.LoadScene("moveTo");
+    }
+}
