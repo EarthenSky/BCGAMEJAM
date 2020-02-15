5c6f47c8958f933cb6cae008cd61f5e99172d1bb
slipperysquid <coleseto123@gmail.com>
2020-02-15 02:08:42 -0800

    health text
    Added player health and writes it to screen

--- Assets/Player/PlayerHealth.cs
+++ Assets/Player/PlayerHealth.cs
@@ -6,15 +6,20 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int dead;
+    public GameObject textBar;
+    private TextMesh hpText;
     void Start()
     {
+        hpText = textBar.GetComponent<TextMesh>();
         health = 100;
         dead = 0;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //prints hp to text
+        hpText.text = "HP: " + health.ToString();
         if(health <= 0)
         {
             dead = 1;
