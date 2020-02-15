b03424c4f38f8be374851d90933a6dd43a32a04a
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-02-15 07:39:31 -0800

    spikes wip

---
+++ Assets/Liver Sec/Map Assets/Spikes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // colliding with a death block will kill the player only
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Player")
+        {
+            Debug.Log("spikes");
+            //kill player here
+            
+        }
+    }
+}
